fd61efa42aaabfbc51f41cf8addeb10f5b31dfcb
symplesims <symplesims@gmail.com>
2025-01-18 00:56:07 +0900

    source / sink 의 기본 동작을 검증 완료 함.

--- debezium-2-cp-kafka/mysql/init.sql
+++ debezium-2-cp-kafka/mysql/init.sql
@@ -9,7 +9,7 @@ FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS simplydemo.users
 (
-    id SERIAL           PRIMARY KEY,
+    id                  SERIAL PRIMARY KEY,
     first_name          VARCHAR(255),
     last_name           VARCHAR(255),
     email               VARCHAR(255),
@@ -17,19 +17,19 @@ CREATE TABLE IF NOT EXISTS simplydemo.users
     lat                 DECIMAL(10, 8),
     lon                 DECIMAL(10, 8),
     created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS simplydemo.products
 (
-    id SERIAL           PRIMARY KEY,
+    id                  SERIAL PRIMARY KEY,
     name                VARCHAR(100),
     description         VARCHAR(500),
     category            VARCHAR(100),
     price               FLOAT,
     image               VARCHAR(200),
     created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/data/products.csv' INTO TABLE simplydemo.products
@@ -38,8 +38,46 @@ OPTIONALLY ENCLOSED BY '"'
 IGNORE 1 LINES
 ( name,description,price,category,image );
 
-LOAD DATA INFILE '/var/lib/mysql-files/data/users.csv' INTO TABLE simplydemo.users
+LOAD DATA INFILE '/var/lib/mysql-files/data/users-mini.csv' INTO TABLE simplydemo.users
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 IGNORE 1 LINES
 ( first_name,last_name,email,residence,lat,lon );
+
+
+CREATE DATABASE IF NOT EXISTS demo;
+use demo;
+
+CREATE USER 'demo'@'%' IDENTIFIED WITH mysql_native_password BY 'demo1234';
+GRANT ALL PRIVILEGES ON demo.* TO 'demo'@'%';
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'demo'@'%';
+
+FLUSH PRIVILEGES;
+
+
+CREATE TABLE IF NOT EXISTS demo.customer
+(
+    id                  SERIAL PRIMARY KEY,
+    first_name          VARCHAR(255),
+    last_name           VARCHAR(255),
+    email               VARCHAR(255),
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS demo.board
+(
+    num                 SERIAL PRIMARY KEY,
+    subject             VARCHAR(255),
+    contents            VARCHAR(500),
+    user_id             BIGINT NOT NULL,
+    last_name           VARCHAR(255),
+    full_name           VARCHAR(255),
+    nick                VARCHAR(255),
+    email               VARCHAR(255),
+    twitter             VARCHAR(255),
+    viewcnt             INT DEFAULT 0,
+    user_type           VARCHAR(10),
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
